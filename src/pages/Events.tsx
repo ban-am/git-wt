969d589c038d6934bf19ef9b022c05c876019c3a
bandik <bandik@NB-BANDIK-CZ>
2020-09-30 12:30:33 +0200

    .

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,7 +1,10 @@
-import React, { useContext, useLayoutEffect } from 'react';
+import React, { useContext } from 'react';
 import { observer } from 'mobx-react-lite';
 import { RootStoreContext } from '../stores/rootStore';
 import { Timeline } from 'antd';
+var Scroll = require('react-scroll');
+
+const { Link, Element } = Scroll;
 
 const Events: React.FC = () => {
     const rootStore = useContext(RootStoreContext);
@@ -19,17 +22,30 @@ const Events: React.FC = () => {
 
     const print = (key: string) => {
         let list = [];
-        list.push(<Timeline.Item color="red"><h2>{key}</h2></Timeline.Item>);
-        list.push(grupedEvents[key].map((x: any, id: any) => <Timeline.Item><pre>{JSON.stringify(x, replacer, 1).slice(2,-2).replace(/\"/g, '')}</pre></Timeline.Item>));
+        list.push(
+            <Timeline.Item color="red"><h2>{key}</h2></Timeline.Item>
+        );
+        list.push(grupedEvents[key].map((x: any, id: any) => <Timeline.Item><pre>{JSON.stringify(x, replacer, 1).slice(2, -2).replace(/\"/g, '')}</pre></Timeline.Item>));
         return list;
     }
 
     return (
         <>
+            <nav className="vertical-nav">
+                <ul>
+                    {Object.keys(grupedEvents).map((key, index) => (
+                        <li key={index}>
+                            <Link activeClass="active" to={key} spy={true} smooth={true} duration={250} >{key}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
             {events && (
                 <Timeline >
                     {Object.keys(grupedEvents).map((key, index) => (
-                        print(key)
+                        <Element name={key} className="element big">
+                           {print(key)}
+                        </Element>
                     ))}
                 </Timeline>
             )}
